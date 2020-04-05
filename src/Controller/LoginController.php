06a13dfb0f9e7e3ab75463a1859c83ec68b3bd14
piotreq7 <45266559+piotreq7@users.noreply.github.com>
2020-04-05 23:23:21 +0200

    fix

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,7 +5,6 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
 
 class LoginController extends AbstractController
 {
@@ -13,16 +12,8 @@ class LoginController extends AbstractController
      * @route("/")
      * @return Response
      */
-    public function show(EntityManagerInterface $em)
+    public function show()
     {
-
-//            $user = new User();
-//            $user->setFirstName('kaska');
-//            $user->setPassword("ttt");
-//            $user->setEmail('333322@o2.pl');
-//            $em->persist($user);
-//            $em->flush();
-
 
 
         return $this->render('frontpage.html.twig');
